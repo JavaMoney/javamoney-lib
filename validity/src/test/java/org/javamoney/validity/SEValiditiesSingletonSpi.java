c02e7d4259906fd10878cb8182d69e3fcaa80712
keilw <werner.keil@gmx.net>
2013-10-08 03:24:07 +0200

    Refactored test package

--- validity/src/test/java/org/javamoney/validity/SEValiditiesSingletonSpi.java
+++ validity/src/test/java/org/javamoney/validity/SEValiditiesSingletonSpi.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.javamoney.ext;
+package org.javamoney.validity;
 
 import java.util.Collection;
 import java.util.Set;
