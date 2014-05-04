2d21928045165ac2be262dca9ec620b5d60e35bd
Anatole Tresch <atsticks@gmail.com>
2014-05-04 09:03:50 +0200

    Merged.

--- calc/src/main/java/org/javamoney/calc/UltraFastMoneyAmountFactory.java
+++ calc/src/main/java/org/javamoney/calc/UltraFastMoneyAmountFactory.java
@@ -2,7 +2,7 @@ package org.javamoney.calc;
 //
 //import javax.money.CurrencyUnit;
 //import javax.money.MonetaryContext;
-//import javax.money.MonetaryContext.AmountFlavor;
+//import javax.money.MonetaryContext.AmountFlavor.java;
 //
 //import org.javamoney.moneta.FastMoney;
 //import org.javamoney.moneta.spi.AbstractAmountFactory;
@@ -24,7 +24,7 @@ package org.javamoney.calc;
 //	@Override
 //	protected MonetaryContext loadDefaultMonetaryContext() {
 //		return new MonetaryContext.Builder(
-//				FastMoney.class).setFlavor(AmountFlavor.PERFORMANCE)
+//				FastMoney.class).setFlavor(AmountFlavor.java.PERFORMANCE)
 //				.setMaxScale(32).setFixedScale(false)
 //				.setPrecision(String.valueOf(Double.MAX_VALUE).length() - 1)
 //				.build();
@@ -35,7 +35,7 @@ package org.javamoney.calc;
 //		return new MonetaryContext.Builder(
 //				FastMoney.class)
 //				.setMaxScale(32).setFixedScale(false)
-//				.setFlavor(AmountFlavor.PERFORMANCE)
+//				.setFlavor(AmountFlavor.java.PERFORMANCE)
 //				.setPrecision(String.valueOf(Double.MAX_VALUE).length() - 1)
 //				.build();
 //	}
