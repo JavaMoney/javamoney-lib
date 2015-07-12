7a1c474bbf4b627854fa3b20a81de931fe8c6cc7
otaviojava <otaviojava@java.net>
2015-07-12 18:05:25 -0300

    Refactor messages to process

--- exchange-rate/update_exchange.sh
+++ exchange-rate/update_exchange.sh
@@ -1,14 +1,14 @@
-printf "\n Downloading the ECB daily resource"
+printf "\n Downloading the ECB daily resource. \n"
 wget http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml
 mv eurofxref-daily.xml exchange-rate-ecb/src/main/resources/java-money/defaults/ECB/eurofxref-daily.xml
-printf "\n Done. Downloading the ECB-90 resource"
+printf "\n Done. Downloading the ECB-90 resource. \n"
 wget http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml
 mv eurofxref-hist-90d.xml exchange-rate-ecb/src/main/resources/java-money/defaults/ECB/eurofxref-hist-90d.xml
-printf "\n Done. Downloading the ECB-hist resource"
+printf "\n Done. Downloading the ECB-hist resource. \n"
 wget http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml
 mv eurofxref-hist.xml exchange-rate-ecb/src/main/resources/java-money/defaults/ECB/eurofxref-hist.xml
 wget http://www.imf.org/external/np/fin/data/rms_five.aspx?tsvflag=Y
-printf "\n Done. Downloading the IMF resource"
+printf "\n Done. Downloading the IMF resource. \n"
 mv rms_five.aspx?tsvflag=Y rms_five.xls
 mv rms_five.xls exchange-rate-imf/src/main/resources/java-money/defaults/IMF/rms_five.xls
-printf "\n Done. Job finished and all exchange rate resources were updated."
+printf "\n Done. Job finished and all exchange rate resources were updated. \n"
