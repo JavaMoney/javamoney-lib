a79feac8070b54e143b59e2aed84e220f8785453
otaviojava <otaviojava@java.net>
2015-07-12 18:03:19 -0300

    Creates script to update exchange rate

---
+++ exchange-rate/update_exchange.sh
@@ -0,0 +1,15 @@
+printf "\n Downloading the ECB daily resource"
+wget http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml
+mv eurofxref-daily.xml exchange-rate-ecb/src/main/resources/java-money/defaults/ECB/eurofxref-daily.xml
+printf "\n Done. Downloading the ECB-90 resource"
+wget http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml
+mv eurofxref-hist-90d.xml exchange-rate-ecb/src/main/resources/java-money/defaults/ECB/eurofxref-hist-90d.xml
+printf "\n Done. Downloading the ECB-hist resource"
+wget http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml
+mv eurofxref-hist.xml exchange-rate-ecb/src/main/resources/java-money/defaults/ECB/eurofxref-hist.xml
+wget http://www.imf.org/external/np/fin/data/rms_five.aspx?tsvflag=Y
+printf "\n Done. Downloading the IMF resource"
+mv rms_five.aspx?tsvflag=Y rms_five.xls
+mv rms_five.xls exchange-rate-imf/src/main/resources/java-money/defaults/IMF/rms_five.xls
+printf "\n Done. Job finished and all exchange rate resources were updated."
+
