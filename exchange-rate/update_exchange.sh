350ba9d1860ed9e04ce03ff40ad038e4b7d67cb5
otaviojava <otaviojava@java.net>
2015-07-14 22:09:40 -0300

    Fixes description on update_exchange script

--- exchange-rate/update_exchange.sh
+++ exchange-rate/update_exchange.sh
@@ -7,8 +7,8 @@ mv eurofxref-hist-90d.xml exchange-rate-ecb/src/main/resources/java-money/defaul
 printf "\n Done. Downloading the ECB-hist resource. \n"
 wget http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist.xml
 mv eurofxref-hist.xml exchange-rate-ecb/src/main/resources/java-money/defaults/ECB/eurofxref-hist.xml
-wget http://www.imf.org/external/np/fin/data/rms_five.aspx?tsvflag=Y
 printf "\n Done. Downloading the IMF resource. \n"
+wget http://www.imf.org/external/np/fin/data/rms_five.aspx?tsvflag=Y
 mv rms_five.aspx?tsvflag=Y rms_five.xls
 mv rms_five.xls exchange-rate-imf/src/main/resources/java-money/defaults/IMF/rms_five.xls
 printf "\n Done. Job finished and all exchange rate resources were updated. \n"
