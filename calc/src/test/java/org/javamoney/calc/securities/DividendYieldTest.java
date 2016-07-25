1aaf4d7b9e71b153515e4f9b529f8ddde0fa841d
Manuela Grindei <manuelag2004@yahoo.co.uk>
2016-07-25 01:19:38 +0100

    refactored dividend yield test

--- calc/src/test/java/org/javamoney/calc/securities/DividendYieldTest.java
+++ calc/src/test/java/org/javamoney/calc/securities/DividendYieldTest.java
@@ -11,8 +11,11 @@ import static junit.framework.Assert.assertEquals;
  */
 public class DividendYieldTest {
 
+    private static final Money DIVIDENDS = Money.of(1.12, "GBP");
+    private static final Money INITIAL_PRICE = Money.of(28, "GBP");
+
     @Test
     public void testCalculate() {
-        assertEquals(0.04, DividendYield.calculate(Money.of(1.12, "GBP"), Money.of(28, "GBP")).doubleValue());
+        assertEquals(0.04, DividendYield.calculate(DIVIDENDS, INITIAL_PRICE).doubleValue());
     }
 }
