5869737309f124b709997364f02bddcdca38b428
Manuela Grindei <manuelag2004@yahoo.co.uk>
2016-07-26 23:25:43 +0100

    refactored bid ask spread test

--- calc/src/test/java/org/javamoney/calc/securities/BidAskSpreadTest.java
+++ calc/src/test/java/org/javamoney/calc/securities/BidAskSpreadTest.java
@@ -11,8 +11,11 @@ import static org.junit.Assert.assertEquals;
  */
 public class BidAskSpreadTest {
 
+    private static final Money ASK_PRICE = Money.of(37.8, "GBP");
+    private static final Money BID_PRICE = Money.of(37.75, "GBP");
+
     @Test
     public void testCalculate() {
-        assertEquals(Money.of(0.05, "GBP"), BidAskSpread.calculate(Money.of(37.8, "GBP"), Money.of(37.75, "GBP")));
+        assertEquals(Money.of(0.05, "GBP"), BidAskSpread.calculate(ASK_PRICE, BID_PRICE));
     }
 }
