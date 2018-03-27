2b230d5115434182089fce57d4e84244d9eabc45
Anatole Tresch <atsticks@gmail.com>
2018-03-27 17:13:17 +0200

    javamoney-lib/issues/38 Fixed livense headers.

--- calc/src/main/java/org/javamoney/calc/common/AbstractRateAndPeriodBasedOperator.java
+++ calc/src/main/java/org/javamoney/calc/common/AbstractRateAndPeriodBasedOperator.java
@@ -1,49 +1,39 @@
 /*
- * Copyright (c) 2012, 2013, Credit Suisse (Anatole Tresch), Werner Keil. Licensed under the Apache
- * License, Version 2.0 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
+ * Copyright (c) 2012, 2018, Werner Keil, Anatole Tresch and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
+ *
+ * Contributors: @atsticks - initial version.
  */
 package org.javamoney.calc.common;
 
-import javax.money.MonetaryAmount;
 import javax.money.MonetaryOperator;
 import java.util.Objects;
 
-import static org.javamoney.calc.CalculationContext.one;
-
 /**
- * <img src= "http://www.financeformulas.net/Formula%20Images/Annuity%20Payment%20(FV)%201.gif" />
- * <p>
- * The annuity payment formula shown above is used to calculate the cash flows of an annuity when
- * future value is known. An annuity is denoted as a series of periodic payments. The annuity
- * payment formula shown here is specifically used when the future value is known, as opposed to the
- * annuity payment formula used when present value is known. There are not only mathematical
- * differences between calculating an annuity when present value is known and when future value is
- * known, but also differences in the real life application of the formulas. For example, if an
- * individual is wanting to calculate the payments on a loan, the original loan balance would be
- * considered the present value and the payment formula would accommodate this known variable.
- * However, if an individual is wanting to calculate how much they need to save per year in an
- * interest bearing account to have a certain balance after a specific period of time, then this
- * wanted balance would be considered the future value. The latter example would use the annuity
- * payment using future value formula as the balance is increasing instead of decreasing:
+ * Base class for a {@link MonetaryOperator} taking a calculatoric rate and period number.
  *
  * @author Anatole Tresch
- * @author Werner Keil
- * @link http://www.financeformulas.net/Annuity-Payment-from-Future-Value.html
  */
 public abstract class AbstractRateAndPeriodBasedOperator implements MonetaryOperator {
 
     /**
-     * the target rate, not null.
+     * The target rate and periods, not null.
      */
-    protected RateAndPeriods rateAndPeriods;
+    protected final RateAndPeriods rateAndPeriods;
 
     /**
-     * Private constructor.
+     * Constructor.
      *
      * @param rateAndPeriods    the target rate and periods, not null.
      */
@@ -51,14 +41,26 @@ public abstract class AbstractRateAndPeriodBasedOperator implements MonetaryOper
         this.rateAndPeriods = Objects.requireNonNull(rateAndPeriods);
     }
 
+    /**
+     * Get the rate and periods.
+     * @return the rate and periods, never null.
+     */
     public RateAndPeriods getRateAndPeriods() {
         return rateAndPeriods;
     }
 
+    /**
+     * Get the current rate.
+     * @return the current rate, never null.
+     */
     public Rate getRate(){
         return rateAndPeriods.getRate();
     }
 
+    /**
+     * Get the current periods, never null.
+     * @return the current periods, >= 0.
+     */
     public int getPeriods(){
         return rateAndPeriods.getPeriods();
     }
@@ -80,8 +82,9 @@ public abstract class AbstractRateAndPeriodBasedOperator implements MonetaryOper
 
     @Override
     public String toString() {
-        return "AbstractRateAndPeriodBasedOperator{" +
-                "rateAndPeriods=" + rateAndPeriods +
+        return getClass().getSimpleName() + "{" +
+                "\n " + rateAndPeriods +
                 '}';
     }
+
 }
--- calc/src/main/java/org/javamoney/calc/common/RateAndPeriods.java
+++ calc/src/main/java/org/javamoney/calc/common/RateAndPeriods.java
@@ -1,7 +1,27 @@
+/*
+ * Copyright (c) 2012, 2018, Werner Keil, Anatole Tresch and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Contributors: @atsticks
+ */
 package org.javamoney.calc.common;
 
 import java.util.Objects;
 
+/**
+ * Data container class that summarizes a calculatoric rate and the number of periods the rate should be applied.
+ */
 public class RateAndPeriods {
 
     /**
@@ -13,6 +33,11 @@ public class RateAndPeriods {
      */
     private final int periods;
 
+    /**
+     * Private constructor.
+     * @param rate the rate, not null.
+     * @param periods the periods, >= 0.
+     */
     private RateAndPeriods(Rate rate, int periods) {
         if(periods<0){
             throw new IllegalArgumentException("Periods must be >= 0");
@@ -21,18 +46,38 @@ public class RateAndPeriods {
         this.rate = Objects.requireNonNull(rate);
     }
 
+    /**
+     * Get a new instance.
+     * @param rate the target rate, not null.
+     * @param periods the periods, >= 0.
+     * @return a new instance.
+     */
     public static RateAndPeriods of(Rate rate, int periods) {
         return new RateAndPeriods(rate, periods);
     }
 
+    /**
+     * Get a new instance.
+     * @param rate the target rate, not null.
+     * @param periods the periods, >= 0.
+     * @return a new instance.
+     */
     public static RateAndPeriods of(double rate, int periods) {
         return new RateAndPeriods(Rate.of(rate), periods);
     }
 
+    /**
+     * Get the current target rate.
+     * @return the target rate, never null.
+     */
     public Rate getRate() {
         return rate;
     }
 
+    /**
+     * The target periods, >= 0.
+     * @return the target periods.
+     */
     public int getPeriods() {
         return periods;
     }
@@ -45,37 +90,4 @@ public class RateAndPeriods {
                 "}";
     }
 
-    public RatePeriodsBuilder toBuilder() {
-        return builder().withRate(rate).withPeriods(periods);
-    }
-
-    public static RatePeriodsBuilder builder() {
-        return new RatePeriodsBuilder();
-    }
-
-    public static class RatePeriodsBuilder {
-
-        private Rate rate;
-        private int periods;
-
-        private RatePeriodsBuilder() {
-        }
-
-        public RatePeriodsBuilder withRate(Rate rate) {
-            this.rate = Objects.requireNonNull(rate);
-            return this;
-        }
-
-        public RatePeriodsBuilder withPeriods(int periods) {
-            if(periods<0){
-                throw new IllegalArgumentException("Periods must be >= 0");
-            }
-            this.periods = periods;
-            return this;
-        }
-
-        public RateAndPeriods build() {
-            return new RateAndPeriods(this.rate, this.periods);
-        }
-    }
 }
