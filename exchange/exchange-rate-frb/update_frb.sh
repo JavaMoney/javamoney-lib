fba7875a9779b7924e12857b402973152e002576
Anatole Tresch <atsticks@gmail.com>
2018-03-27 16:41:53 +0200

    javamoney-lib/issues/20 Replaced download scripts with maven download plugin.

--- exchange/exchange-rate-frb/update_frb.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-printf "\n Downloading the Yahoo resource. \n"
-wget http://www.federalreserve.gov/feeds/data/H10_H10.XML
-mv H10_H10.XML src/main/resources/java-money/defaults/FRB/H10_H10.XML
-printf "\n Done. \n"
--- exchange/exchange-rate-yahoo/update_yahoo.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-printf "\n Downloading the Yahoo resource. \n"
-wget http://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote
-mv quote src/main/resources/java-money/defaults/YAHOO/finance.xml
-printf "\n Done. \n"
